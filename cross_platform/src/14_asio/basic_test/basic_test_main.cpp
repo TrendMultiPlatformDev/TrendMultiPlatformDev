efa046a56b203b42513cd281883017c61768a701
dabaosod011 <dabaosod011@gmail.com>
2014-03-19 13:57:02 +0800

    Update 14_asio basic test

--- cross_platform/src/14_asio/basic_test/basic_test_main.cpp
+++ cross_platform/src/14_asio/basic_test/basic_test_main.cpp
@@ -5,14 +5,18 @@
 #include <boost/asio.hpp>
 #include <boost/asio/ip/address.hpp>
 
-BOOST_AUTO_TEST_CASE( asio_test ) 
-{
-	int i = 1;
-	BOOST_ASSERT(i == 1);
-}
+BOOST_AUTO_TEST_SUITE(s_asio_ip);
 
-BOOST_AUTO_TEST_CASE( asio_ip_test )
+BOOST_AUTO_TEST_CASE( asio_ip_testcase1 ) 
 {
 	boost::asio::ip::address addr = boost::asio::ip::address::from_string("127.0.0.1");
 	BOOST_ASSERT( addr.is_loopback() == true );
 }
+
+BOOST_AUTO_TEST_CASE( asio_ip_testcase2 )
+{
+	boost::asio::ip::address addr = boost::asio::ip::address_v4::loopback();
+	BOOST_ASSERT( addr.to_string() == "127.0.0.1" );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
