aefa1a4382dc41f84ff5dbbc108301864f9cdc2a
Holley Hu <leihu0724@126.com>
2014-05-07 20:46:13 +0800

    Modify some files by Holley.

--- cross_platform/src/09_algorithm_math/math_case.cpp
+++ cross_platform/src/09_algorithm_math/math_case.cpp
@@ -25,11 +25,14 @@ BOOST_AUTO_TEST_CASE(cstdint_case)
 	i16 = 65534;
 	i32 = 2147483646;
 
-	BOOST_CHECK_EQUAL(typeid(u8).name(), "unsigned char");
+	//cout << "type of u8 is: " << typeid(u8).name() << endl;
 	BOOST_ASSERT(sizeof(u8) == 1);
-	BOOST_CHECK_EQUAL(typeid(i16).name(), "int");
-	BOOST_ASSERT(sizeof(i16) == 4);
-	BOOST_CHECK_EQUAL(typeid(i32).name(), "int");
+
+	//cout << "type of i16 is: " << typeid(i16).name() << endl;
+	//BOOST_ASSERT(sizeof(i16) == 4);
+	//cout << sizeof(i16) << endl;
+
+	//cout << "type of i32 is:" << typeid(i32).name() << endl;
 	BOOST_ASSERT(sizeof(i32) == 4);
 }
 
@@ -37,72 +40,73 @@ BOOST_AUTO_TEST_CASE(integer_case)
 {
 	//int_fast_t
 	int_fast_t<int>::fast a;
-	BOOST_CHECK_EQUAL(typeid(a).name(), "int");
+	BOOST_ASSERT(sizeof(a) == 4);
 
 	//int_t, uint_t
 	int_t<32>::fast b;
-	BOOST_CHECK_EQUAL(typeid(b).name(), "int");
+	BOOST_ASSERT(sizeof(b) == 4);
 
 	uint_t<15>::fast c;
-	BOOST_CHECK_EQUAL(typeid(c).name(), "unsigned short");
+	BOOST_ASSERT(sizeof(c) == 2);
 
 	//int_max_value_t, int_min_value_t,uint_value_t
 	int_max_value_t<65536>::fast d;
-	BOOST_CHECK_EQUAL(typeid(d).name(), "int");
+	BOOST_ASSERT(sizeof(d) == 4);
 
 	int_min_value_t<-65536>::fast e;
-	BOOST_CHECK_EQUAL(typeid(e).name(), "int");
+	BOOST_ASSERT(sizeof(e) == 4);
 
 	uint_value_t<33000>::fast f;
-	BOOST_CHECK_EQUAL(typeid(f).name(), "unsigned short");
+	BOOST_ASSERT(sizeof(f) == 2);
 }
 
 BOOST_AUTO_TEST_CASE(crc_case)
 {
 	crc_32_type crc32;
-	cout << hex;
-	cout << crc32.checksum() << endl;
+	//cout << hex;
+	//cout << crc32.checksum() << endl;
 
 	crc32.process_byte('a');
-	cout << "checksum: " << crc32.checksum() << endl;
-	cout << "operator: " << crc32() << endl;
+	//cout << "checksum: " << crc32.checksum() << endl;
+	//cout << "operator: " << crc32() << endl;
 
 	crc32.process_bytes("0123456789", 10);
-	cout << crc32.checksum() << endl;
+	//cout << crc32.checksum() << endl;
 
 	char szCh[] = "0123456789";
 	crc32.reset();
 
 	crc32.process_block(szCh, szCh + 10);
-	cout <<crc32.checksum() << endl;
+	//cout <<crc32.checksum() << endl;
 
 	crc32.reset();
 	string str = "1234567890";
-	cout << for_each(str.begin(), str.end(), crc32)() << endl;
-	cout << "last:" << crc32() << endl;
+	//cout << for_each(str.begin(), str.end(), crc32)() << endl;
+	//cout << "last:" << crc32() << endl;
 }
 
 
 BOOST_AUTO_TEST_CASE(random_case)
 {
 	mt19937 rng(time(0));
-	cout << mt19937::min() << "<->" << mt19937::max() << endl;
-	cout << endl;
-	for (int i=0; i<10; ++i)
+	cout << "min & max of random: " << mt19937::min() << "<->" << mt19937::max() << endl;
+	for (int i=0; i<5; ++i)
 	{
 		cout << rng() << ",";
 	}
 	cout << endl;
+
 	uniform_int<> ui(0, 255);
-	for (int i=0; i<10; ++i)
+	for (int i=0; i<5; ++i)
 	{
 		cout << ui(rng) << ",";
 	}
 	cout << endl;
 
 	uniform_01<> u01;
-	for (int i=0; i<10; ++i)
+	for (int i=0; i<5; ++i)
 	{
 		cout << u01(rng) << ",";
 	}
+	cout << endl;
 }
